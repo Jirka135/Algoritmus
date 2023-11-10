bfd2b006794df186bf0a0c7d7a366c366a55c420
Jirka135 <54354077+Jirka135@users.noreply.github.com>
2023-11-10 09:43:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import random
 import matplotlib
-matplotlib.use('agg')  # new line
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
@@ -14,21 +13,22 @@ def index():
 
 @app.route('/plot', methods=['POST'])
 def plot():
-    #definice seznamů
+    # Definition of lists
     serazene = []
     n_cisla = []
 
-    #req z formuláře
+    # Request data from the form
     nejmensi = int(request.form['nejmensi'])
     nejvetsi = int(request.form['nejvetsi'])
     cisel = int(request.form['cisel'])
-    #naplnění
+
+    # Function to fill the list with random numbers
     def napl_cisly(cisla,c1,c2):
         while len(cisla) < cisel:
             cisla.append(random.randint(c1,c2))
+    napl_cisly(n_cisla, nejmensi, nejvetsi)
 
-    napl_cisly(n_cisla,nejmensi,nejvetsi)
-    #sorting
+    # Sorting
     for c in n_cisla:
         if not serazene:
             serazene.append(c)
@@ -39,17 +39,22 @@ def plot():
                     break
             else:
                 serazene.append(c)
-    #labely
+    # Labels
     labels = list(set(serazene))
+    
+    # Create the plot
     plt.clf()
-    plt.bar(labels, [serazene.count(x) for x in labels],label='čísla')
+    plt.bar(labels, [serazene.count(x) for x in labels], label='čísla')
     plt.title('Graf čísel')
 
-    #buffer
     buf = BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
     plot_url = base64.b64encode(buf.getvalue()).decode()
 
+    plt.close()  # Close the figure to avoid issues
 
     return render_template('form.html', plot_url=plot_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
